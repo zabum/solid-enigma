6063c2d61b76726bb1b0cb64de5b0d5c8ea50147
zabum <demir.musab@gmail.com>
2017-04-27 17:35:38 +0200

    notes

---
+++ NotizMgr/NotizMgr/Models/Termin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace NotizMgr.Data
+{
+    public class Termin : Element
+    {
+        [XmlElement("Inhalt")]
+        public string m_szInhalt { get; set; }
+
+        public Termin() { }
+
+        public Termin(eElementType l_eType, string l_szInhalt) : base(l_eType)
+        {
+            m_szInhalt = l_szInhalt;
+        }
+
+        public override string ToString()
+        {
+            return m_szInhalt;
+        }
+    }
+}
---
+++ NotizMgr/NotizMgr/Views/InputView.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static NotizMgr.Data.Element;
+
+namespace NotizMgr.Views
+{
+    public partial class InputView : Form
+    {
+        public Data.Element m_cElement { get; private set; }
+        InputViewModel m_cInput { get; set; }
+        
+        public InputView(eElementType l_eType)
+        {
+            InitializeComponent();
+            m_cInput = new InputViewModel(l_eType);
+            SetCaption();
+        }
+
+        private void SetCaption()
+        {
+            lblCaption.Text = m_cInput.GetElementType();
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            m_cElement = m_cInput.CreateElement(txtContent.Text);
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
